ff80d3572a22bfa8f0925beeedbab2ae89e7ba86
Haberno <52333828+Haberno@users.noreply.github.com>
2026-09-09 23:37:12 -0400

    fix(cli): honor screenshot delays beyond five frames

--- src/WallpaperEngine/Application/ApplicationContext.cpp
+++ src/WallpaperEngine/Application/ApplicationContext.cpp
@@ -702,8 +702,6 @@ void ApplicationContext::loadSettingsFromArgv () {
 	}
 
 	this->settings.audio.volume = std::max (0, std::min (this->settings.audio.volume, 128));
-	this->settings.screenshot.delay
-	    = std::max<uint32_t> (0, std::min<uint32_t> (this->settings.screenshot.delay, 5));
 
 	// use std::cout on this in case logging is disabled, this way it's easy to look at what is running
 	std::stringbuf buffer;
